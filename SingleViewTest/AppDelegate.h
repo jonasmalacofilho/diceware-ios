788eb57f10e0f296ddf6abca201a16f773739e69
Jonas Malaco Filho <jonas@jonasmalaco.com>
2015-10-08 12:15:28 -0300

    Initial Commit

---
+++ SingleViewTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SingleViewTest
+//
+//  Created by Jonas Malaco Filho on 10/8/15.
+//  Copyright © 2015 Elebeta. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SingleViewTest/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SingleViewTest
+//
+//  Created by Jonas Malaco Filho on 10/8/15.
+//  Copyright © 2015 Elebeta. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
