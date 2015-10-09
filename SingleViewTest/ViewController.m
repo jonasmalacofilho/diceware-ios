21630c9aecacd47de83a9a7c4509aed416cbbc73
Jonas Malaco Filho <jonas@jonasmalaco.com>
2015-10-08 21:10:10 -0300

    Initial English Diceware using arc4random

--- SingleViewTest/ViewController.m
+++ SingleViewTest/ViewController.m
@@ -6,22 +6,37 @@
 //  Copyright © 2015 Elebeta. All rights reserved.
 //
 
+#include <stdlib.h>
 #import "ViewController.h"
 
 @interface ViewController ()
-
+@property NSArray* words;
+@property (weak, nonatomic) IBOutlet UITextField *wordBox;
 @end
 
 @implementation ViewController
 
+- (void)loadDictionary {
+    NSString* path = [[NSBundle mainBundle] pathForResource:@"data" ofType:@"txt"];
+    NSLog(@"dictionary asset path: %@", path);
+    NSString* data = [NSString stringWithContentsOfFile:path];
+    self.words = [data componentsSeparatedByString:@"\n"];
+}
+
 - (void)viewDidLoad {
     [super viewDidLoad];
+    [self loadDictionary];
     // Do any additional setup after loading the view, typically from a nib.
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)generate:(UIButton *)sender {
+    int r = arc4random_uniform(7776);
+    self.wordBox.text = self.words[r];
 }
 
 @end
